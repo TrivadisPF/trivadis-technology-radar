2566a246fe9ee1dc67720e8190e172e3c2b94d94
gschmutz <guido.schmutz@trivadis.com>
2021-04-15 15:35:43 +0200

    Revert "update to latest version"
    This reverts commit 1d8ca8ed1a103d769a928825f0cfaf69ae402399.

--- src/date.ts
+++
@@ -1,5 +0,0 @@
-import moment from 'moment';
-
-const isoDateToMoment = (isoDate: moment.MomentInput) => moment(isoDate, 'YYYY-MM-DD');
-
-export const formatRelease = (isoDate: moment.MomentInput) => isoDateToMoment(isoDate).format('MMMM YYYY');
